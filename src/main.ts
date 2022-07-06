b5da9c73dd72774ba8fda8454cf819ddf0889b04
matsuyou <mtk4091@yahoo.co.jp>
2022-07-06 23:35:27 +0900

    デプロイ環境調整

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+import { LAppDelegate } from './lappdelegate';
+import * as LAppDefine from './lappdefine';
+
+/**
+ * ブラウザロード後の処理
+ */
+window.onload = (): void => {
+  // create the application instance
+  if (LAppDelegate.getInstance().initialize() == false) {
+    return;
+  }
+
+  LAppDelegate.getInstance().run();
+};
+
+/**
+ * 終了時の処理
+ */
+window.onbeforeunload = (): void => LAppDelegate.releaseInstance();
+
+/**
+ * Process when changing screen size.
+ */
+window.onresize = () => {
+  if (LAppDefine.CanvasSize === 'auto') {
+    LAppDelegate.getInstance().onResize();
+  }
+};
+
+/**
+ * Virtiual Reaction Additional Code
+ * guiからの命令に応じて処理
+ **/
+window.addEventListener("message", function(event: any) {
+	if(event.origin == "http://localhost:8000"){
+    LAppDelegate.onEventListened(event.data);
+	}
+},false);
